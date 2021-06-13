1303e96cf185432127a45723aece3a10d3ca7fc0
altruios <49083558+altruios@users.noreply.github.com>
2021-06-12 17:01:48 -0700

    working!!!!!

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -5,9 +5,11 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+# define M_PI           3.14159265358979323846  /* pi */
 using namespace std; 
-constexpr int _height=50;
-constexpr int _width =50;
+constexpr int _height=270;
+constexpr int _width =480;
+constexpr int _scale = 8;
 
 
 class Cell{
@@ -15,7 +17,7 @@ class Cell{
           int life=0;
           int x;
           int y;
-          int c=0;
+          int c=1;
           Cell* neighbors[8];
           Cell(int _x,int _y){
                this->x=_x;
@@ -27,14 +29,11 @@ class Cell{
           int get_life_count(){
                return c;
           }
-          int set_life_get_count(int _Life){
-               this->color_counter_step(_Life);
-               this->life = _Life;
-               return this->c;
+          void set_color(int _c){
+               this->c=_c;
           }
           void set_life(int _Life){
                this->life = _Life;
-               this->color_counter_step(_Life);
           }
           void set_n(Cell* n1,Cell* n2,Cell* n3,Cell* n4,Cell* n5,Cell* n6,Cell* n7,Cell* n8){
                this->neighbors[0]=n1;
@@ -64,7 +63,6 @@ class Cell{
                int n = this->get_n();
                int l = this->get_life();
                int g = (l==1)?(n>3||n<2)?0:1:(n==3)?1:0;
-               this->color_counter_step(g);
                return g;
           }
 };
@@ -73,6 +71,7 @@ class Board{
      public:
      int height=_height;
      int width=_width;
+     int scale=_scale;
      vector<vector<Cell>> matrix_A;
      vector<vector<Cell>> matrix_B;
      vector<vector<vector<unsigned char>>>  matrix_C;
@@ -97,24 +96,21 @@ class Board{
           this->set_neighbors();
      }  
      void set_neighbors(){
-          for (int y = 0; y < this->height-1; y++){
-               for (int x = 0; x < this->width-1; x++){
-                    int wrappedYUnder=  (y-1)<0              ? this->height-1 :y-1;
-                    int wrappedYOver =  (y+1)>this->height-1 ? 0              :y+1;
-                    int wrappedXUnder=  (x-1)<0              ? this->width-1  :x-1;
-                    int wrappedXOver =  (x+1)>this->width-1  ? 0              :x+1;
-
-                    Cell* n1=this->get_cell_by_XY(x,wrappedYUnder);
-                    Cell* n2=this->get_cell_by_XY(wrappedXOver,wrappedYUnder);
-                    Cell* n3=this->get_cell_by_XY(wrappedXOver,y);
-                    Cell* n4=this->get_cell_by_XY(wrappedXOver,wrappedYOver);
-                    Cell* n5=this->get_cell_by_XY(x,wrappedYOver);
-                    Cell* n6=this->get_cell_by_XY(wrappedXUnder,wrappedYOver);
-                    Cell* n7=this->get_cell_by_XY(wrappedXUnder,(y));
-                    Cell* n8=this->get_cell_by_XY(wrappedXUnder,wrappedYUnder);
-
+          for (int y = 0; y < this->height; y++){
+               for (int x = 0; x < this->width; x++){
+                    int wrappedYUnder = (y + this->height - 1) % this->height;
+                    int wrappedYOver  = (y + this->height + 1) % this->height;
+                    int wrappedXUnder = (x + this->width - 1)   % this->width;
+                    int wrappedXOver  = (x + this->width + 1)  % this->width;
+                    Cell* n1=this->get_cell_by_XY(x,wrappedYUnder);//top
+                    Cell* n2=this->get_cell_by_XY(wrappedXOver,wrappedYUnder);//top right
+                    Cell* n3=this->get_cell_by_XY(wrappedXOver,y);//right
+                    Cell* n4=this->get_cell_by_XY(wrappedXOver,wrappedYOver);//bottom right
+                    Cell* n5=this->get_cell_by_XY(x,wrappedYOver);//bottom
+                    Cell* n6=this->get_cell_by_XY(wrappedXUnder,wrappedYOver);//bottom left
+                    Cell* n7=this->get_cell_by_XY(wrappedXUnder,(y));//left
+                    Cell* n8=this->get_cell_by_XY(wrappedXUnder,wrappedYUnder);//top left
                     this->flip_matrix();
-
                     Cell* nb1=this->get_cell_by_XY(x,wrappedYUnder);
                     Cell* nb2=this->get_cell_by_XY(wrappedXOver,wrappedYUnder);
                     Cell* nb3=this->get_cell_by_XY(wrappedXOver,y);
@@ -126,15 +122,15 @@ class Board{
 
                     this->flip_matrix();
 
-                    this->matrix_A[x][y].set_n(n1,n2,n3,n4,n5,n6,n7,n8);
-                    this->matrix_B[x][y].set_n(nb1,nb2,nb3,nb4,nb5,nb6,nb7,nb8);    
+                    this->matrix_A[y][x].set_n(n1,n2,n3,n4,n5,n6,n7,n8);
+                    this->matrix_B[y][x].set_n(nb1,nb2,nb3,nb4,nb5,nb6,nb7,nb8);    
                }
           }
           cout<< "neighbors set"<<endl;  
      }
      Cell* get_cell_by_XY(int x, int y){
-          //the refference of the cell at [y][x] of the the matrix of pointer
-          return &(*get_current_matrix())[y][x];
+          //the address of the cell at [y][x] of the the matrix of pointer
+          return &((*get_current_matrix())[y][x]);
      }
      vector<std::vector<Cell>>* get_current_matrix(){
           if(this->current_matrix_is_a==true){
@@ -152,13 +148,15 @@ class Board{
           this->current_matrix_is_a = !this->current_matrix_is_a;
      }
      void step(){
-          cout<<"we take a step:"<<endl;
           vector<vector<Cell>>* current_matrix = this->get_current_matrix();
           vector<vector<Cell>>* next_matrix = this->get_next_matrix();
-          for(int y=0;y<this->height-1;y++){
-               for(int x=0;x<this->width-1;x++){
+          for(int y=0;y<this->height;y++){
+               for(int x=0;x<this->width;x++){
                     int l = (*current_matrix)[y][x].game_of_life();
-                    int c= (*next_matrix)[y][x].set_life_get_count(l);
+                    (*current_matrix)[y][x].color_counter_step(l);
+                    int c = (*current_matrix)[y][x].get_life_count();
+                    (*next_matrix)[y][x].set_life(l);
+                    (*next_matrix)[y][x].set_color(c);
                     this->set_pixel(x,y,l,c);
                }    
           }
@@ -173,39 +171,32 @@ class Board{
           string function_name = "render_images";
           for(int file_count =0;file_count<render_count;file_count++){
                auto start = std::chrono::high_resolution_clock::now();
-               this->step();
                this->write_file(file_count);
+               this->step();
                auto stop = std::chrono::high_resolution_clock::now();
                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-               cout<<function_name << "  which took: "<<duration.count() << "\n";
+               auto count_left= render_count-file_count;
+               auto estimation  = duration.count()*count_left;
+               cout<<function_name << "  which took: "<<duration.count() <<"  eta:"<<estimation<< "\r";
           }
      }
      void write_file(int i){
           char file_name[50];
           int file_name_length;
           file_name_length=sprintf(file_name, "images\\game_of_life_test_%05d.ppm\0",  i);
-          char file_test_name[50];
-          int file_test_name_length;
-          file_test_name_length=sprintf(file_test_name,"tests\\game_of_life_test_%d.txt\0",i);              
           FILE *file = fopen(file_name, "wb");      
-          ofstream test_file(file_test_name);
-          test_file<< "render test: ", i, "\n\n";
-          fprintf(file, "P6\n%d %d\n255\n", this->width-1, this->height-1);
-          test_file<<"[\n";
-          for(int y =0;y<this->height-1;y++){
-               test_file<< " [";
-               for(int x =0;x<this->width-1;x++){
-                    this->load_color(x,y);
-                    fwrite(this->_color,1,3, file);
-                    int l = (*this->get_current_matrix())[y][x].get_life();
-                    int other_l = (*this->get_next_matrix())[y][x].get_life();
-                    test_file<< "(x:"<< x<<" y:"<< y<<" c_m:"<<l<<", n_m:: "<<other_l<<"),";
+          fprintf(file, "P6\n%d %d\n255\n", (this->width-1)*(this->scale), (this->height-1)*(this->scale));
+               for(int y =0;y<this->height-1;y++){
+                    for(int i=0;i<this->scale;i++){
+                         for(int x =0;x<this->width-1;x++){
+                              for(int j=0;j<this->scale;j++){
+                              this->load_color(x,y);
+                              fwrite(this->_color,1,3, file);
+                         }
+                    }
                }
-               test_file<<"],\n";
           }
-          test_file<<"]";
           fclose(file);
-          test_file.close();
      }
      void load_color(int x,int y){
           this->_color[0]=this->matrix_C[y][x][0];  
@@ -214,17 +205,16 @@ class Board{
      }
      void render_video(){
           //todo - python scipt call/replace.
-          std::string command = "python3 imgToVid.py";
+          std::string command = "python imgToVid.py";
 
           FILE *python_render_script= popen(command.c_str(),"r");
           fscanf(python_render_script, command.c_str());
      }
      void set_pixel(int x, int y,int l,int c){
           if(l){
-
-               this->matrix_C[y][x][0]=255;
-               this->matrix_C[y][x][1]=255;
-               this->matrix_C[y][x][2]=255;
+               this->matrix_C[y][x][0]=floor(150*sin((c)/M_PI) +155);
+               this->matrix_C[y][x][1]=floor(150*tan((c*2)/M_PI)+155);
+               this->matrix_C[y][x][2]=floor(150*cos((c*3)/M_PI)+155);
                
                }else{  
                this->matrix_C[y][x][0]=0;
@@ -232,14 +222,13 @@ class Board{
                this->matrix_C[y][x][2]=0;
                
                }
-     }
+     } 
      void set_random_pixel(){
           int x=rand()%(this->width-1);
           int y=rand()%(this->height-1);
           this->set_pixel(x,y,1,1);
           (*this->get_current_matrix())[y][x].set_life(1);
           (*this->get_next_matrix())[y][x].set_life(1);
-
      }
      void set_pixel_and_matrixies(int x, int y){
           
@@ -248,28 +237,42 @@ class Board{
           (*this->get_next_matrix())[y][x].set_life(1);
 
      }
+     void set_neighbors_of_XY(int x,int y){
+           for(Cell* c:this->matrix_A[y][x].neighbors){
+               c->set_life(1);
+               this->set_pixel(c->x,c->y,1,1);
 
+           }
+          for(Cell* c:this->matrix_B[y][x].neighbors){
+               c->set_life(1);
+               this->set_pixel(c->x,c->y,1,1);
+
+           }
+     }
+     void add_glider(){
+          int x=rand()%(this->width-40)+20;
+          int y=rand()%(this->height-40) +20;
+          vector<vector<int>> glider {{x,y},{x,y+1},{x,y+2},{x-1,y+2},{x-2,y+1}};
+          for(vector<int> item: glider){
+               t.set_pixel_and_matrixies(item[0],item[1]);    
+          }
+     }
 };
+
+
+
 
 int main(){
      cout << "making game of life:" << endl;
      Board t;
      cout<<"board made"<<endl;
-     vector<vector<int>> test_points {
-          {0,0},{1,0},{2,0},{16,10},{17,10},{18,10},{10,12},{11,12},{12,12},{13,12},
-          {14,12}, {_width-1,_height-1},{40,40},{40,41},{40,42},{41,42},{42,41},{31,31},{31,32},{31,33}
-     };
-     for(vector<int> item: test_points){
-     t.set_pixel_and_matrixies(item[0],item[1]);    
+     for(int i=0;i<4200;i++){
+          t.add_glider();
      }
-     Cell* test = t.get_cell_by_XY(25,25);
-     for(Cell* n : test->neighbors){
-          n->set_life(1);
+     for(int video_file_count =0;video_file_count<50;video_file_count++)
+          {      
+          t.render(5000);
      }
-     t.write_file(100); 
-          
-          t.render(100);
-
      cout<< "completed" <<endl;
      cout<<"closing in: ";
      cout<<"\b \b3";
